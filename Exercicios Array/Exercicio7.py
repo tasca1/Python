b67112564b89712fa9fc3bc5f4c9549fcf65fd19
Vitor Tasca da Costa <165608558+tasca1@users.noreply.github.com>
2024-09-10 20:18:36 -0300

    Delete Exercicios Array/Exercicio7.py

--- Exercicios Array/Exercicio7.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-lista = np.array([float(input(f"Digite o {i+1} número: ")) for i in range(10)])
-
-maior_valor = np.max(lista)
-posicao_maior_valor = np.argmax(lista)
-
-print("O maior valor é: ", maior_valor)
-print("A posição dele é: ", posicao_maior_valor)
